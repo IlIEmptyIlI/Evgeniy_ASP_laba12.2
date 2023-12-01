54822508efe589c447357bc06c22543db6fc3525
IlIEmptyIlI <132489781+IlIEmptyIlI@users.noreply.github.com>
2023-12-02 00:00:45 +0200

    Add files via upload

---
+++ Laba12.2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Laba12_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Laba12_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var companies = _context.Companies.ToList();
+            return View(companies);
+        }
+    }
+}
---
+++ Laba12.2/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Laba12_2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Company> Companies { get; set; }
+}
